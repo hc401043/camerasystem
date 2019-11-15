3f09880d2b54e89301c526effd783d9dce1253c4
hikichi <m.hikichi.1969@gmail.com>
2019-11-15 11:58:00 +0900

    git add¤򤷤Ƥ¤¤ʤ«¤ä¿¡£ ¥֥í¥å¯¼±Ê̻þ¤ˤÏÂ礭¤µÀ©¸¤¬ÉÔÍפǤ뤳¤ȤËÂбþ¡£

--- ai/ainumber.py
+++ ai/ainumber.py
@@ -145,7 +145,8 @@ class AiNumber:
         dst = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
 
         #数値の閾値処理
-        ret, dst = cv2.threshold(dst, 130, 255, cv2.THRESH_BINARY)  #170 大きくすると拾いやすい
+        threth = 140 #170 大きくすると拾いやすい
+        ret, dst = cv2.threshold(dst, threth, 255, cv2.THRESH_BINARY)  
         #モルフォルジ処理
         #kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
         #dst = cv2.erode(dst,kernel,iterations = 18)
